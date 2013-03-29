730b3a92f9b8ece9a17d21aa9331a7f4b98f12dd
parimal jariwala <parimal@lampvb.(none)>
2013-03-29 14:43:09 +0530

    Fetched records from database and displayed using jade template

--- myapp/models/user.js
+++ myapp/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'), Schema = mongoose.Schema;
 var UserSchema = new Schema({
+  _id: Number,
   name: {first: String, last: String}
 });
 mongoose.model('User', UserSchema);
