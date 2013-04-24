e1633fb5e6bf0900894e72a50aee91e1f3a69be9
parimal jariwala <parimal@lampvb.(none)>
2013-04-24 18:16:10 +0530

    Complete example of Add/Edit/Delete users

--- myapp/controllers/users.js
+++ myapp/controllers/users.js
@@ -2,9 +2,7 @@ var mongoose = require('mongoose')
   , User = mongoose.model('User');
 
 exports.list = function(req, res) {
-	//res.render('user',{title:'Users List'});
   User.find({}, function(err, users) {
-  //console.log(users);
   res.render('user', {
       title: 'Manage Users',
       users: users,
@@ -19,27 +17,34 @@ exports.add = function (req, res) {
   var user = new User({ _id: req.body._id, name: {first: req.body.first, last: req.body.last} });
   user.save(function (err) {
     if(err) {
-      console.log(err)
+      console.log(err);
     } else {
       res.redirect('/users');
-      //console.log(user);
     }
  });
 }
 
+exports.update = function (req, res) {
+  User.findOneAndUpdate({_id : req.params.uid}, {name: {first: req.body.first, last: req.body.last}},
+  function(err, user) {
+    res.redirect('/users');
+  });
+}
+
+exports.del = function (req, res) {
+  User.findByIdAndRemove({_id : req.params.uid},
+  function(err, user) {
+    res.redirect('/users');
+  });
+}
+
 exports.findById = function (req, res) {
- //res.send(req.params.uid);
- User.findOne({ _id: req.params.uid }, function (err, users) {
+  User.findOne({ _id: req.params.uid }, function (err, user) {
       res.render('user', {
-      _id: users._id,
-      firstname: users.name.first,
-      lastname: users.name.last,
+      _id: user._id,
+      firstname: user.name.first,
+      lastname: user.name.last,
       title: 'Manage Users',
     });
- });
- User.find({}, function(err, users) {
-    res.render('user', {
-      users: users,
-   });
  });
 }
--- myapp/routes.js
+++ myapp/routes.js
@@ -4,11 +4,7 @@ var phonebook = require('./controllers/phonebook');
   app.get('/users', users.list);
   app.post('/users/add', users.add);
   app.get('/users/:uid', users.findById);
+  app.post('/users/update/:uid', users.update);
+  app.get('/users/delete/:uid', users.del);
   app.get('/phones', phonebook.list);
-  /*app.get('/api', function (req,res) {
-    var obj = {
-      spam: 'test'
-    }
-    res.send(obj);
-  });*/
 }
